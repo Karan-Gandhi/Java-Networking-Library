199f94ff4ccfdb5fc9ba37bf02c5184eb64baf99
Karan Gandhi <karangandhi.programming@gmail.com>
2021-01-25 20:22:49 +0530

    completed the whole documentation and removed all warnings

--- src/main/java/com/karangandhi/networking/utils/Tasks.java
+++ src/main/java/com/karangandhi/networking/utils/Tasks.java
@@ -6,21 +6,41 @@ import com.karangandhi.networking.core.Task;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * This contains all the tasks that will be used be the server and the client
+ */
 public class Tasks {
+    /**
+     * This is the readMessage task that is assigned by the connection to the Owner Context
+     */
     public static class ReadMessageTask extends Task {
-        public static interface Callback {
+        /**
+         * This is the callback that is called when a message appears on the input stream
+         */
+        public interface Callback {
             void onMessageReceived(Message<?, ?> message);
         }
 
-        public static interface DisconnectCallback {
+        /**
+         * This is the callback that is called when the server or the client closes the connection
+         */
+        public interface DisconnectCallback {
             void onDisconnect();
         }
 
-        private InputStream inputStream;
-        private Callback readMessageCallback;
-        private DisconnectCallback disconnect;
+        final private InputStream inputStream;
+        final private Callback readMessageCallback;
+        final private DisconnectCallback disconnect;
         public boolean isAlive;
 
+        /**
+         * Creates a instance of the ReadMessageTask
+         *
+         * @param context           The context of the Task
+         * @param inputStream       The inputStream from which the messages must be read
+         * @param callback          The callback that is called when a message is recieved
+         * @param disconnect        The callback that is called when the server or the client closes the connection
+         */
         public ReadMessageTask(Context context, InputStream inputStream, Callback callback, DisconnectCallback disconnect) {
             super(true, context);
             this.inputStream = inputStream;
@@ -29,20 +49,31 @@ public class Tasks {
             this.isAlive = true;
         }
 
+        /**
+         * Runs the task
+         *
+         * @throws IOException      Thrown when there is a error reading the message
+         */
         @Override
         public void run() throws IOException {
             while (isAlive) {
                 synchronized (this.inputStream) {
-                    Message newMessage = Message.readFrom(this.inputStream);
+                    Message<?, ?> newMessage = Message.readFrom(this.inputStream);
                     readMessageCallback.onMessageReceived(newMessage);
                 }
             }
         }
 
+        /**
+         * This will colas the connection if there is a error while reading
+         *
+         * @param e     The error while reading
+         * @return      True in all cases
+         */
         @Override
         public boolean onComplete(Exception e) {
             if (e != null) disconnect.onDisconnect();
-            return false;
+            return true;
         }
     }
 }
